e79ffb8acec1f24344b441ff5a920c3e30f6c60b
somecallmejosh <josh@thebrileys.com>
2025-01-17 12:39:19 -0500

    Fix linting errors.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -56,6 +56,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `
   } catch (error) {
+    console.error(error)
     return {
       message: 'Database Error: Failed to Create Invoice.',
     }
@@ -93,6 +94,7 @@ export async function updateInvoice(
       WHERE id = ${id}
     `
   } catch (error) {
+    console.error(error)
     return {
       message: 'Database Error: Failed to Create Invoice.',
     }
